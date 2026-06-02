f81a7dc7cb222129a1b893ec84c81e742a0cbcf1
Dark <martin45melo@gmail.com>
2026-06-02 07:23:59 -0500

    #00 - Python

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/Dark.py
@@ -0,0 +1,23 @@
+# https://www.python.org/
+"""python
+# Also, this is  a comment
+#
+#
+#
+#
+# And multiple lines
+"""
+
+# Variables
+a = 2
+
+# Las variables en mayúsculas se entienden por variables que no deben modificarse
+# PATH = /ruta/al/archivo/destino
+
+entero = 4
+flotante = 3.5
+booleano = True
+texto = "Hola python"
+
+
+print(texto)
